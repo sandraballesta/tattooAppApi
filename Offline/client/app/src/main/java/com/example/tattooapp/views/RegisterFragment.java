f8307dd998822123f029bfd56a8ec602831d8772
Marcel Gimeno <marcel.gimeno@adevinta.com>
2021-06-01 23:32:02 +0200

    Refactor RegisterFragment

--- Offline/client/app/src/main/java/com/example/tattooapp/views/RegisterFragment.java
+++ Offline/client/app/src/main/java/com/example/tattooapp/views/RegisterFragment.java
@@ -20,57 +20,55 @@ import java.io.IOException;
 
 public class RegisterFragment extends Fragment {
 
-    RegisterController controller;
-    private Button btnToLogin;
-    private Button btnRegister;
-  /*  private TextInputEditText email;
-    private TextInputEditText  password;
-    private TextInputEditText  name;*/
-    private String name;
-    private String email;
-    private String password;
-    private View view;
+  private RegisterController controller;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        this.view = inflater.inflate(R.layout.fragment_register, container, false);
-        setLayout();
-        btnToLogin.setOnClickListener(v -> {
-            Navigation.findNavController(view).navigate(R.id.toLogin);
-        });
+  private Button btnToLogin;
+  private Button btnRegister;
 
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    try {
-                        TextInputLayout vemail = view.findViewById(R.id.email);
-                        email = vemail.getEditText().getText().toString().trim();
-                        TextInputLayout vpassword = view.findViewById(R.id.password);
-                        password = vpassword.getEditText().getText().toString().trim();
-                        TextInputLayout vname = view.findViewById(R.id.name);
-                        name = vname.getEditText().getText().toString().trim();
+  private TextInputEditText email;
+  private TextInputEditText password;
+  private TextInputEditText name;
 
-                        controller.registerUser(name, email, password);
-                    } catch (IOException e) {
-                        Log.i("TRYING TO REGISTER", "whats up");
-                        e.printStackTrace();
-                    }
-                }
-        });
+  private View view;
 
-        return this.view;
-    }
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    this.view = inflater.inflate(R.layout.fragment_register, container, false);
 
+    controller = new RegisterController(this);
 
+    setLayout();
 
-    private void setLayout(){
-        controller = new RegisterController(this);
-        btnToLogin = view.findViewById(R.id.btnToLogin);
-        btnRegister = view.findViewById(R.id.btnRegister);
+    btnToLogin.setOnClickListener(v -> {
+      Navigation.findNavController(view).navigate(R.id.toLogin);
+    });
 
+    btnRegister.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        try {
+          String emailValue = email.getEditText().getText().toString().trim();
+          String passwordValue = password.getEditText().getText().toString().trim();
+          String nameValue = name.getEditText().getText().toString().trim();
 
+          controller.registerUser(nameValue, emailValue, passwordValue);
+        } catch (IOException e) {
+          Log.i("TRYING TO REGISTER", "whats up");
+          e.printStackTrace();
+        }
+      }
+    });
 
-    }
+    return this.view;
+  }
+
+  private void setLayout() {
+    btnToLogin = view.findViewById(R.id.btnToLogin);
+    btnRegister = view.findViewById(R.id.btnRegister);
+    email = view.findViewById(R.id.email);
+    password = view.findViewById(R.id.password);
+    name = view.findViewById(R.id.name);
+  }
 }
